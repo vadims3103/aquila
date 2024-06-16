70e590f6c49a375633e80c097e8675a65c3bd046
vadims3103 <vadims3103@yahoo.com>
2024-06-17 02:17:49 +0300

    first commit

---
+++ index.php
@@ -0,0 +1,19 @@
+<?php
+/**
+ * Main template file.
+ * 
+ * @package Aquila
+ */
+ ?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>WordPress Theme</title>
+</head>
+<body>
+    hello Wordpress
+</body>
+</html>
