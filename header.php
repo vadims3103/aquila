9606a9d8820b7c57386d47b28b08e57c6d1ef712
vadims3103 <vadims3103@yahoo.com>
2024-06-18 00:19:15 +0300

    New files added

---
+++ header.php
@@ -0,0 +1,20 @@
+<?php
+/**
+ * Header template
+ * 
+ * @package Aquila
+ */
+?>
+
+<!DOCTYPE html>
+<html lang="<?php language_attributes(); ?>">
+<head>
+    <meta charset="<?php bloginfo( 'charset' ); ?>">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <?php wp_head(); ?>
+</head>
+<body <?php body_class(); ?>>
+
+    <?php wp_body_open(); ?>
+
+    <header>Header</header>
--- index.php
+++ index.php
@@ -4,17 +4,13 @@
  * 
  * @package Aquila
  */
+
+ get_header();
  ?>
 
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>WordPress Theme</title>
-</head>
-<body>
-    hello Wordpress !!!
-</body>
-</html>
+<div class="content">
+    Content
+</div>
 
+<?php 
+ get_footer(); 
